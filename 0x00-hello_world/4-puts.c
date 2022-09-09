204122d2db9846183d18130490063391cb5423c4
eliwarrie <eelitetech9@gmail.com>
2022-09-09 05:53:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * Task no 4-puts.c this is a comment!
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print = "Programming is like building a multilingual puzzle", using puts.
+ *
+ * Return (0) normal.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
