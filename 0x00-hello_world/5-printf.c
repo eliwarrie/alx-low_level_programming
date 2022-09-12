793c99b3a73bcc23355f549a84a667f839c4497c
eliwarrie <eelitetech9@gmail.com>
2022-09-12 02:22:32 -0700

    modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
 * main - Print the string in the put function
 *
 * Description: using the main function
-* this program will "Display the string inside quotations
+* this program prints "Display the string inside quotations
 * Return 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
