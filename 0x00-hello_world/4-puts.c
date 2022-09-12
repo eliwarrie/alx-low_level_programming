c79683712006bd3ce19f8be6f0ea03f50050d68f
eliwarrie <eelitetech9@gmail.com>
2022-09-12 01:38:57 -0700

    modify, 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-/*
+#include <stdio.h>
+/**
  * Task no 4-puts.c this is a comment!
  *
  *
  * main - print = "Programming is like building a multilingual puzzle", using puts.
  *
- * Return (0) normal.
+ * Return 0
  */
-#include <stdio.h>
-
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-
 	return 0;
 }
