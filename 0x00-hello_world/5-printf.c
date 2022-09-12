34eb0aec6ee133fe9bcc4fdbf4bea0571e894645
eliwarrie <eelitetech9@gmail.com>
2022-09-12 02:14:05 -0700

    modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
- *
- * Return (0); as usual.
- */
+* main - Print the string in the put function
+*
+* Description: using the main function
+* this program will "Display the string inside quotations
+* Return 0
+*/
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
