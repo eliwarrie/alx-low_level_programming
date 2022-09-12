fc8596f7b11385e01898dc1922d9b5ee6815bc12
eliwarrie <eelitetech9@gmail.com>
2022-09-12 01:56:06 -0700

    4-puts.c, Use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
-* Task no 4-puts.c this is a comment!
-*
-*
-* main - this program will print "Programming is like building a multilingual puzzle
-*
-* Return 0
+* main - Entry point
+* Return: Always 0
 */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
