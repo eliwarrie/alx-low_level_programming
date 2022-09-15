c8229fe80f45fc6616d856fc518c3189d5f18092
eliwarrie <eelitetech9@gmail.com>
2022-09-14 17:12:33 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
