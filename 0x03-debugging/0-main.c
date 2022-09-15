e8580c3c7e905f2d78193b096c6f76a2e1a1ee08
eliwarrie <eelitetech9@gmail.com>
2022-09-15 10:56:42 -0700

    0-main.c positive or negative return task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#incluide "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negation
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
