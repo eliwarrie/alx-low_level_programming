b5c4477162ecebdf32b53ada41cda02e318d60a0
eliwarrie <eelitetech9@gmail.com>
2022-09-16 03:23:40 -0700

    modify code

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,14 +2,14 @@
 #define MAIN_H
 
 /**
- * main - header file for prototypes
- * void prototymes (void)
- * int prototypes (int)
- */
+* main - header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
 
 void print_numbers(void);
 void print_most_numbers(void);
-void more_numbers(void)
+void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
