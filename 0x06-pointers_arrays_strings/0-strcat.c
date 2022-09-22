48c4bd7eb4746b9985a8cd5ee0de9658a701052b
eliwarrie <eelitetech9@gmail.com>
2022-09-21 21:16:59 -0700

    0-strcat.c task file release

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates the string pointed to by @src to
+* the end of string pointed to @dest
+*@dest: String that will be appended
+*@src: String to be concatenated upon
+*
+* Return: return pointer to @dest
+*/
+
+char *_strcat(char *dest, char *src);
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
