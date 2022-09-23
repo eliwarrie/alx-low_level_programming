d317f5a964d599f450a2f2fe6b1adf4cd9d2e95b
eliwarrie <eelitetech9@gmail.com>
2022-09-22 20:02:31 -0700

    101-print_number.c task modify release

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,15 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	ni = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		num = -num;
 	}
 
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
