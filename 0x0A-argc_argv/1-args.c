9e672d189ae5c7ee9135895d1f8b7d8991b817c0
eliwarrie <eelitetech9@gmail.com>
2022-10-02 08:31:33 -0700

    File: 1-args.c release

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints the name number of arguments passed to main
- * @argc: number of line argument passed
- * @argv: argument vector of pointers to strings
+ * main - prints the number of arguments passed to main
+ * @argc: argument counter
+ * @argv: array of pointer to strings
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	int i;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
