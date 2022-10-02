2d82378f9eb4ed216e2e91f6e497a9d54cfeadba
eliwarrie <eelitetech9@gmail.com>
2022-10-02 05:37:11 -0700

    File: 3-mul.c release

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,23 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main - program that prints its name, followed by a new line
+ * main - program that multiply two numbers  new line
  * @argc: argument that contains argument input
  * @argv: argument that stores the strings in an array of char* (strings)
- * Return: 0
+ * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-	int num_1, num_2 mul;
-
-	if (arg != 3)
-		printf("Error\n");
-	else
+	if (argc != 3)
 	{
-		num_1 = atoi(argv[1]);
-		num_2 = atoi(argv[2]);
-		mul = num_1 * num_2;
-		printf("%d\n", mul);
+		printf("Error\n" ,);
+		return (1);
 	}
-	return 0;
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
