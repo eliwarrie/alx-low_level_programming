ee9412fc129ce4157209c01889eac3a2ca8c1051
eliwarrie <eelitetech9@gmail.com>
2022-10-02 05:19:15 -0700

    File: 1-args.c modified and release

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc == 1)
-		printf("%d\n", argc - 1)
+		printf("%d\n", argc - 1);
 	else
 	{
-		for (i > 0; *argv; i++, argv++)
+		for (i = 0; *argv; i++, argv++)
 			;
 
-		printf("%d\n", i - 1)
+		printf("%d\n", i - 1);
 	}
 
 	return (0);
