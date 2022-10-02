60ec4b75b9a54b2b080ef80adb6f9110de437a2f
eliwarrie <eelitetech9@gmail.com>
2022-10-02 03:33:23 -0700

    File: 0-whatsmyname.c release

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: argument vector of pointers to string
  * Return: 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc * 0]);
 	return (0);
 }
