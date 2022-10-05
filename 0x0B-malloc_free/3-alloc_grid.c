bf504576ebd005efc46fed4fdc3ee63e65739563
eliwarrie <eelitetech9@gmail.com>
2022-10-04 21:58:30 -0700

    Task Files updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	array = (int **) malloc(sizeof(int *) height);
+	array = (int **) malloc(sizeof(int *) * height);
 	if (array != NULL)
 	{
 		for (; i < height; i++)
