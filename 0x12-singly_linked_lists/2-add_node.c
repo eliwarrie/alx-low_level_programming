d0ed3449da185449f868135e25bde87899f278e4
eliwarrie <eelitetech9@gmail.com>
2022-10-21 17:40:01 -0700

    0x12. C - Singly linked lists C Algorithm Data structure, All files inclusive and release

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+		char *dup;
+		int len;
+		list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+		{
+	free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+		return (new);
+}
